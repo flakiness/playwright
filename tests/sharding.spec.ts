b4c757701a74019b1abc6eee3370e15b2572e105
Andrey Lushnikov <aslushnikov@gmail.com>
2026-05-29 21:23:39 -0400

    test: add test for dependent projects

--- tests/sharding.spec.ts
+++ tests/sharding.spec.ts
@@ -58,6 +58,36 @@ test('should shard tests without historical durations', async ({}, testInfo) => 
   expect(shards.map(shard => reportTestCount(shard.report)).sort()).toEqual([2, 2]);
 });
 
+test('should generate perfect shards with dependent projects', async ({}, testInfo) => {
+  const shards = await runPerfectShards(testInfo, {
+    'setup.spec.ts': `
+      import { test } from '@playwright/test';
+
+      test('w=100 setup', async () => {});
+    `,
+    'app.spec.ts': `
+      import { test } from '@playwright/test';
+
+      test('w=1 alpha', async () => {});
+      test('w=1 beta', async () => {});
+    `,
+    'unit.spec.ts': `
+      import { test } from '@playwright/test';
+
+      for (let i = 0; i < 100; ++i)
+        test('w=1 unit-test-' + i, async () => {});
+    `,
+  }, 2, {}, {
+    projects: [
+      { name: 'setup', testMatch: 'setup.spec.ts' },
+      { name: 'app', testMatch: 'app.spec.ts', dependencies: ['setup'] },
+      { name: 'unit', testMatch: 'unit.spec.ts', },
+    ],
+  });
+
+  expect(shards.map(shard => shard.totalWeight)).toEqual([101, 201]);
+});
+
 function reportTestCount(report: FlakinessReport.Report): number {
   let count = 0;
   ReportUtils.visitTests(report, test => count += test.attempts.length);
